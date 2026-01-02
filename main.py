ad0853aacb26dff276cb2ffa0f96396e78616662
fedya <fedya.3.4.6.2.7@gmail.com>
2026-01-02 22:36:19 +0200

    edit main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import asyncio
-import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from config import BOT_TOKEN
-from handlers import accounts, channels, forwarding, links
+from handlers import accounts, channels, links, forwarding
+from logger import logger
 
 
 async def main():
-    logging.basicConfig(level=logging.INFO)
+    logger.info("Запуск бота")
     
     bot = Bot(token=BOT_TOKEN)
-    storage = MemoryStorage()
-    dp = Dispatcher(storage=storage)
+    dp = Dispatcher(storage=MemoryStorage())
     
-    dp.include_router(links.router)
-    dp.include_router(channels.router)
     dp.include_router(accounts.router)
+    dp.include_router(channels.router)
+    dp.include_router(links.router)
     dp.include_router(forwarding.router)
     
     await bot.delete_webhook(drop_pending_updates=True)
